b765524e68b26f180b9ddaae5ce2b4bf5fe1848e
Josh Martin <joshua.martin1@edu.sait.ca>
2025-05-20 17:01:06 -0600

    push first code

--- App.tsx
+++ App.tsx
@@ -1,10 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Pressable } from 'react-native';
 
 export default function App() {
+  let sampleList = [
+    {id: 1, name: "Josh"},
+    {id: 2, name: "Thomas"},
+    {id: 3, name: "Kishan"},
+    {id: 4, name: "Mayur"},
+  ];
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
+      {sampleList.map((item)=> (
+        <Text key={item.id} style={{fontSize: 20}}>
+          {item.name}
+        </Text>
+      ))}
+      <Button title ="Hello There" onPress={()=> alert ("You are done with the lab")} />
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +29,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  button: {
+    backgroundColor: "blue",
+    padding: 10,
+    borderRadius: 5,
+  }
 });
